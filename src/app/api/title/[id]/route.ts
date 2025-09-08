9089cb37754f375e0b1c570ae10c692acdbb8fd2
Ogooluwa David Ilori <69655851+Bossogo@users.noreply.github.com>
2025-09-08 11:48:11 +0100

    Update route.ts

--- src/app/api/title/[id]/route.ts
+++ src/app/api/title/[id]/route.ts
@@ -2,11 +2,12 @@ import { NextRequest, NextResponse } from "next/server";
 
 const BASE_URL = "https://api.imdbapi.dev";
 
-export async function GET(
-  _req: NextRequest,
-  context: { params: Record<string, string> }
-) {
-  const { id } = context.params;
+type RouteContext = {
+  params: Promise<{ id: string }>;
+};
+
+export async function GET( _req: NextRequest, context: RouteContext) {
+  const { id } = await context.params;
   const apiRes = await fetch(`${BASE_URL}/titles/${encodeURIComponent(id)}`);
   const data = await apiRes.json();
   return NextResponse.json(data);
